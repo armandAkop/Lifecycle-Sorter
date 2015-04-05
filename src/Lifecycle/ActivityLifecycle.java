1a4950f12f3e528e0284ff159d595e2ada5fd2a1
Armand Akopian <armand.akopyan@gmail.com>
2015-04-05 15:54:54 -0700

    Moved onRestart to be placed before onStart

--- src/Lifecycle/ActivityLifecycle.java
+++ src/Lifecycle/ActivityLifecycle.java
@@ -13,11 +13,11 @@ public class ActivityLifecycle extends Lifecycle {
      * Activity Lifecycle method names
      */
     private static final String ON_CREATE = "onCreate";
+    private static final String ON_RESTART = "onRestart";
     private static final String ON_START = "onStart";
     private static final String ON_RESUME = "onResume";
     private static final String ON_PAUSE = "onPause";
     private static final String ON_STOP = "onStop";
-    private static final String ON_RESTART = "onRestart";
     private static final String ON_DESTROY = "onDestroy";
 
 
@@ -28,11 +28,11 @@ public class ActivityLifecycle extends Lifecycle {
 
     static {
         ACTIVITY_LIFECYCLE_METHODS.add(ON_CREATE);
+        ACTIVITY_LIFECYCLE_METHODS.add(ON_RESTART);
         ACTIVITY_LIFECYCLE_METHODS.add(ON_START);
         ACTIVITY_LIFECYCLE_METHODS.add(ON_RESUME);
         ACTIVITY_LIFECYCLE_METHODS.add(ON_PAUSE);
         ACTIVITY_LIFECYCLE_METHODS.add(ON_STOP);
-        ACTIVITY_LIFECYCLE_METHODS.add(ON_RESTART);
         ACTIVITY_LIFECYCLE_METHODS.add(ON_DESTROY);
     }
 
